8df7ad442c85d2308714f2857ffca6f95571765f
Diogo Vicente <51659633+diogosvicente@users.noreply.github.com>
2023-09-23 13:22:12 -0300

    :lipstick: feat: Apresentando o Expanded

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,43 +27,75 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              "Ações do usuário",
-              style: GoogleFonts.acme(fontSize: 20),
-            ),
-            Text(
-              "Foi clicado $quantidadeCliques vezes",
-              style: GoogleFonts.acme(fontSize: 20),
-            ),
-            Text(
-              "O número gerado foi: $numeroGerado",
-              style: GoogleFonts.acme(fontSize: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.red,
-                  child: Text(
-                    "10",
-                    style: GoogleFonts.acme(fontSize: 20),
-                  ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.cyan,
+                child: Text(
+                  "Ações do usuário",
+                  style: GoogleFonts.acme(fontSize: 20),
                 ),
-                Container(
-                  color: Colors.blue,
-                  child: Text(
-                    "20",
-                    style: GoogleFonts.acme(fontSize: 20),
-                  ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                width: double.infinity,
+                color: Colors.orange,
+                child: Text(
+                  "Foi clicado $quantidadeCliques vezes",
+                  style: GoogleFonts.acme(fontSize: 20),
                 ),
-                Container(
-                  color: Colors.green,
-                  child: Text(
-                    "30",
-                    style: GoogleFonts.acme(fontSize: 20),
-                  ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.indigo,
+              child: Text(
+                "O número gerado foi: $numeroGerado",
+                style: GoogleFonts.acme(fontSize: 20),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.red,
+                        child: Text(
+                          "Nome:",
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.blue,
+                        child: Text(
+                          "Diogo Vicente",
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.green,
+                        child: Text(
+                          "30",
+                          style: GoogleFonts.acme(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             )
           ],
         ),
