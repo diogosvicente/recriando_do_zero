399e8360e9932ca22339ad996050d6453d1c3e1c
Diogo Vicente <51659633+diogosvicente@users.noreply.github.com>
2023-09-26 23:03:36 -0300

    :lipstick: feat: Conhecendo RadioList e Repository

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,18 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/repositories/nivel_repositorie.dart';
 
-class DadosCadastraisPage extends StatelessWidget {
-  final String texto;
-  final List<String> dados;
-  const DadosCadastraisPage(
-      {super.key, required this.texto, required this.dados});
+import '../shared/widgets/text_label.dart';
+
+class DadosCadastraisPage extends StatefulWidget {
+  const DadosCadastraisPage({super.key});
+
+  @override
+  State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
+}
+
+class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
+  var nomeController = TextEditingController(text: "");
+  var dataNascimentoController = TextEditingController(text: "");
+  DateTime? dataNascimento;
+  var nivelRepository = NivelRepository();
+  var niveis = [];
+  var nivelSelecionado = "";
+
+  @override
+  void initState() {
+    niveis = nivelRepository.retornaNiveis();
+    super.initState();
+  }
+
+  /*Text returnText(String texto) {
+    return (Text(texto,
+        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700)));
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(texto),
-      ),
-      body: Center(child: Text(dados.length.toString())),
-    );
+        appBar: AppBar(
+          title: const Text("Meus Dados"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TextLabel(texto: "Nome"),
+              TextField(controller: nomeController),
+              const SizedBox(height: 10),
+              const TextLabel(texto: "Data de Nascimento"),
+              TextField(
+                controller: dataNascimentoController,
+                readOnly: true,
+                onTap: () async {
+                  var data = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime(2000, 1, 1),
+                      firstDate: DateTime(1900, 1, 1),
+                      lastDate: DateTime(2023, 10, 23));
+                  if (data != null) {
+                    dataNascimentoController.text = data.toString();
+                    dataNascimento = data;
+                  }
+                },
+              ),
+              const TextLabel(texto: "Nível de experiência"),
+              Column(
+                  children: niveis
+                      .map((nivel) => RadioListTile(
+                          dense: true,
+                          title: Text(nivel),
+                          selected: nivelSelecionado == nivel,
+                          value: nivel,
+                          groupValue: nivelSelecionado,
+                          onChanged: (value) {
+                            print(value);
+                            setState(() {
+                              nivelSelecionado = value.toString();
+                            });
+                          }))
+                      .toList()),
+              TextButton(
+                  onPressed: () {
+                    debugPrint(nomeController.text);
+                    print(dataNascimento);
+                  },
+                  child: const Text("Salvar"))
+            ],
+          ),
+        ));
   }
 }
