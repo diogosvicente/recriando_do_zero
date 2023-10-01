c8eaf9707c819ecba38d1f55f168cdbf9d1df4d8
Diogo Vicente <51659633+diogosvicente@users.noreply.github.com>
2023-10-01 00:44:50 -0300

    :lipstick: feat: Lista de tarefas - Alterando get e set na model

--- lib/model/tarefa.dart
+++ lib/model/tarefa.dart
@@ -7,23 +7,37 @@ class Tarefa {
 
   Tarefa(this._descricao, this._concluido);
 
-  String getId() {
-    return _id;
-  }
+  String get id => _id;
 
-  String getDescricao() {
-    return _descricao;
-  }
+  // String getId() {
+  //   return _id;
+  // }
 
-  void setDescricao(String descricao) {
+  String get descricao => _descricao;
+
+  // String getDescricao() {
+  //   return _descricao;
+  // }
+
+  set descricao(String descricao) {
     _descricao = descricao;
   }
 
-  bool getConcluido() {
-    return _concluido;
-  }
+  // void setDescricao(String descricao) {
+  //   _descricao = descricao;
+  // }
 
-  void setConcluido(bool concluido) {
+  bool get concluido => _concluido;
+
+  // bool getConcluido() {
+  //   return _concluido;
+  // }
+
+  set concluido(bool concluido) {
     _concluido = concluido;
   }
+
+  // void setConcluido(bool concluido) {
+  //   _concluido = concluido;
+  // }
 }
--- lib/pages/tarefa_page.dart
+++ lib/pages/tarefa_page.dart
@@ -72,7 +72,7 @@ class _TarefaPageState extends State<TarefaPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
+                  const Text(
                     "Apenas não concluídos",
                     style: TextStyle(fontSize: 18),
                   ),
@@ -93,17 +93,16 @@ class _TarefaPageState extends State<TarefaPage> {
                   var tarefa = _tarefas[index];
                   return Dismissible(
                     onDismissed: (DismissDirection dismissDirection) async {
-                      await tarefaRepository.remover(tarefa.getId());
+                      await tarefaRepository.remover(tarefa.id);
                       obterTarefas();
                     },
-                    key: Key(tarefa.getId()),
+                    key: Key(tarefa.id),
                     child: ListTile(
-                      title: Text(tarefa.getDescricao()),
+                      title: Text(tarefa.descricao),
                       trailing: Switch(
-                          value: tarefa.getConcluido(),
+                          value: tarefa.concluido,
                           onChanged: (bool value) async {
-                            await tarefaRepository.alterar(
-                                tarefa.getId(), value);
+                            await tarefaRepository.alterar(tarefa.id, value);
                             obterTarefas();
                           }),
                     ),
